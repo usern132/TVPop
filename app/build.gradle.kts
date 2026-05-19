356b84993b5685131662dda2834480f88642bacd
usern132 <24841687+usern132@users.noreply.github.com>
2026-05-19 15:16:02 +0200

    Activar R8 para la versión de lanzamiento

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
